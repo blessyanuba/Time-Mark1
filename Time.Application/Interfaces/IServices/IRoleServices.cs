a055d94e20e284baa6e3dfa5d5e440e5946caef8
blessyanuba <blessyanuba@gmail.com>
2026-03-16 11:56:54 +0530

    Initial commit

---
+++ Time.Application/Interfaces/IServices/IRoleServices.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Time.Application.Interfaces.IServices
+{
+    internal class IRoleServices
+    {
+    }
+}
---
+++ Time.Infrastructure/DbContext/EasyDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Time.Infrastructure.DbContext
+{
+    internal class EasyDbContext
+    {
+    }
+}
